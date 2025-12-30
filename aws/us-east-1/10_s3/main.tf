15d1516daec0b0c4969518f2a8ead31171bd4495
Mostafa Elnagar <mostafaelnagar.ds@gmail.com>
2025-12-30 02:19:27 +0200

    Update S3 infrastructure: Modify bucket configuration and outputs

--- aws/us-east-1/10_s3/main.tf
+++ aws/us-east-1/10_s3/main.tf
@@ -19,32 +19,10 @@ module "landing_zone_bucket" {
   tags                  = local.default_tags
 }
 
-module "augmented_zone_bucket" {
+module "lakehouse_bucket" {
   source              = "terraform-aws-modules/s3-bucket/aws"
   acceleration_status = "Suspended"
-  bucket              = "${local.name_prefix}-augmented"
-
-  # S3 bucket-level Public Access Block configuration
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-
-  server_side_encryption_configuration = {
-    rule = {
-      apply_server_side_encryption_by_default = {
-        sse_algorithm     = "AES256"
-      }
-    }
-  }
-  expected_bucket_owner = data.aws_caller_identity.current.account_id
-  tags                  = local.default_tags
-}
-
-module "curated_zone_bucket" {
-  source              = "terraform-aws-modules/s3-bucket/aws"
-  acceleration_status = "Suspended"
-  bucket              = "${local.name_prefix}-curated"
+  bucket              = "${local.name_prefix}-lakehouse"
 
   # S3 bucket-level Public Access Block configuration
   block_public_acls       = true
--- aws/us-east-1/10_s3/outputs.tf
+++ aws/us-east-1/10_s3/outputs.tf
@@ -8,22 +8,12 @@ output "s3_landing_zone_bucket_arn" {
   value       = module.landing_zone_bucket.s3_bucket_arn
 }
 
-# Archived Zone Bucket
-output "s3_augmented_zone_bucket_id" {
-  description = "The name of the bucket"
-  value       = module.augmented_zone_bucket.s3_bucket_id
+# Lakehouse Bucket
+output "s3_lakehouse_bucket_id" {
+  description = "The name of the lakehouse bucket"
+  value       = module.lakehouse_bucket.s3_bucket_id
 }
-output "s3_augmented_zone_bucket_arn" {
-  description = "The ARN of the bucket formatted as (arn:aws:s3:::bucket-name)"
-  value       = module.augmented_zone_bucket.s3_bucket_arn
-}
-
-# Curated Zone Bucket
-output "s3_curated_zone_bucket_id" {
-  description = "The name of the bucket"
-  value       = module.curated_zone_bucket.s3_bucket_id
-}
-output "s3_curated_zone_bucket_arn" {
-  description = "The ARN of the bucket formatted as (arn:aws:s3:::bucket-name)"
-  value       = module.curated_zone_bucket.s3_bucket_arn
+output "s3_lakehouse_bucket_arn" {
+  description = "The ARN of the lakehouse bucket formatted as (arn:aws:s3:::bucket-name)"
+  value       = module.lakehouse_bucket.s3_bucket_arn
 }
